6734f8755f3ab1440280936c49a880189cc13df9
popmk <popmk@yopmail.com>
2011-05-13 14:46:32 +0000

    works

--- fonction.js
+++ fonction.js
@@ -9,6 +9,16 @@ now.name = 'Pop';
 var currentgroup = "KPIuser";
 
 
+         
+$(document).ready(function() {
+   now.Log("ok");
+    
+    $('.customLink').live('click', function() {
+       navigator.notification.alert("onload");
+    });
+});
+
+
 function description() {
     document.getElementById('label').value = 'Device Name: '     + device.name     + '<br />' + 
 	'Device PhoneGap: ' + device.phonegap +  
@@ -85,4 +95,35 @@ function kpimaxup(){
 	document.getElementById('kpimax').innerHTML = "KPImax = " + now.KPImax;
 	gauge.limhaute.setValue(now.KPImax);
 };
+
+
+function add_liKPI(txt,id) {     
+    var oUl = document.getElementById('listloadkpi'); 
+	var oLi = document.createElement("li"); 
+	var oText = document.createTextNode(txt); 		
+	var b = document.createElement("a");
+	var div = document.createElement("div");
+	var div2 = document.createElement("div");
+	b.setAttribute('id',id);
+	b.setAttribute('href',"#");
+	b.appendChild(oText); 
+	oUl.appendChild(oLi); 
+	oLi.appendChild(div);
+	div.setAttribute('class','ui-btn-inner');
+	div.appendChild(div2);
+	b.setAttribute('class','ui-link-inherit');
+	div2.setAttribute('class','ui-btn-text');
+	div2.appendChild(b);
+	oLi.setAttribute('class','ui-btn ui-btn-up-c ui-btn-icon-right ui-li');
+	return b;
+}
+
+
+function listgroup(){
+  navigator.notification.alert("listgroup");
+   for(i=0; i < now.listgroup.length ; i ++){
+    add_liKPI(now.listgroup[i],"li");   
+   }
+    
+}
 
